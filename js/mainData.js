12d8e03368ff024c1eea2e8d4eff6dc5916404a4
AV <av@anastasia.io>
2017-11-07 15:53:34 -0800

    data model updates

--- js/mainData.js
+++ js/mainData.js
@@ -1,7 +1,7 @@
 module.exports = {
     "locations": {
-        "00": {
-            "name": "Origin",
+        "origin": {
+            "title": "Origin",
             "description": "A hole to see the sky through",
             "coord": {
                 "x": 0,
@@ -9,8 +9,8 @@ module.exports = {
                 "z": 0
             }
         },
-        "01": {
-            "name": "South",
+        "interiorSouth": {
+            "title": "South",
             "description": "The path to civilization",
             "coord": {
                 "x": 0,
@@ -18,8 +18,8 @@ module.exports = {
                 "z": -6
             }
         },
-        "02": {
-            "name": "East",
+        "interiorEast": {
+            "title": "East",
             "description": "The sun rises",
             "coord": {
                 "x": 6,
@@ -27,8 +27,8 @@ module.exports = {
                 "z": 0
             }
         },
-        "03": {
-            "name": "North",
+        "interiorNorth": {
+            "title": "North",
             "description": "To eternity",
             "coord": {
                 "x": 0,
@@ -36,8 +36,8 @@ module.exports = {
                 "z": 6
             }
         },
-        "04": {
-            "name": "West",
+        "interiorWest": {
+            "title": "West",
             "description": "Dusk over mountains",
             "coord": {
                 "x": -6,
@@ -45,11 +45,8 @@ module.exports = {
                 "z": 0
             }
         },
-
-
-
-        "10": {
-            "name": "South Gate",
+        "southGate": {
+            "title": "South Gate",
             // "description": "",
             "coord": {
                 "x": 0,
@@ -57,17 +54,17 @@ module.exports = {
                 "z": -34
             }
         },             
-        "11": {
-            "name": "Into the fold",
-            // "description": "",
+        "southCourtyard": {
+            "title": "Into the fold",
+            "description": "",
             "coord": {
                 "x": 0,
                 "y": 0,
                 "z": -24
             }
         },
-        "12": {
-            "name": "First Contact",
+        "southCantilever": {
+            "title": "First Contact",
             // "description": "",
             "coord": {
                 "x": 0,
@@ -75,8 +72,8 @@ module.exports = {
                 "z": -14
             }
         },
-        "13": {
-            "name": "South Entry",
+        "southEntry": {
+            "title": "South Entry",
             // "description": "",
             "coord": {
                 "x": 0,
@@ -84,8 +81,8 @@ module.exports = {
                 "z": -9
             }
         },
-        "14": {
-            "name": "North Entry",
+        "northEntry": {
+            "title": "North Entry",
             // "description": "",
             "coord": {
                 "x": 0,
@@ -93,8 +90,8 @@ module.exports = {
                 "z": 9
             }
         },
-        "15": {
-            "name": "The Edge of Civilization",
+        "northCantilever": {
+            "title": "The Edge of Civilization",
             // "description": "",
             "coord": {
                 "x": 0,
@@ -102,8 +99,8 @@ module.exports = {
                 "z": 14
             }
         },         
-        "16": {
-            "name": "North Gate",
+        "northGate": {
+            "title": "North Gate",
             // "description": "",
             "coord": {
                 "x": 0,
@@ -111,11 +108,8 @@ module.exports = {
                 "z": 24
             }
         },
-
-
-
-        "20": {
-            "name": "West Gate",
+        "westGate": {
+            "title": "West Gate",
             // "description": "",
             "coord": {
                 "x": -29,
@@ -123,8 +117,8 @@ module.exports = {
                 "z": 0
             }
         },
-        "21": {
-            "name": "Ritual",
+        "westCourtyard": {
+            "title": "Ritual",
             // "description": "",
             "coord": {
                 "x": -20,
@@ -132,8 +126,8 @@ module.exports = {
                 "z": 0
             }
         },
-        "22": {
-            "name": "Sunset Through the Trees",
+        "westCantilever": {
+            "title": "Sunset Through the Trees",
             // "description": "",
             "coord": {
                 "x": -14,
@@ -141,8 +135,8 @@ module.exports = {
                 "z": 0
             }
         },
-        "23": {
-            "name": "West Entry",
+        "westEntry": {
+            "title": "West Entry",
             // "description": "",
             "coord": {
                 "x": -9,
@@ -150,8 +144,8 @@ module.exports = {
                 "z": 0
             }
         },
-        "24": {
-            "name": "East Entry",
+        "eastEntry": {
+            "title": "East Entry",
             // "description": "",
             "coord": {
                 "x": 9,
@@ -159,8 +153,8 @@ module.exports = {
                 "z": 0
             }
         },
-        "25": {
-            "name": "Dawn in the Forest",
+        "eastCantilever": {
+            "title": "Dawn in the Forest",
             // "description": "",
             "coord": {
                 "x": 14,
@@ -168,8 +162,8 @@ module.exports = {
                 "z": 0
             }
         },
-        "26": {
-            "name": "Introspection",
+        "eastCourtyard": {
+            "title": "Introspection",
             // "description": "",
             "coord": {
                 "x": 20,
@@ -177,8 +171,8 @@ module.exports = {
                 "z": 0
             }
         },
-        "27": {
-            "name": "East Gate",
+        "eastGate": {
+            "title": "East Gate",
             // "description": "",
             "coord": {
                 "x": 29,
@@ -186,86 +180,69 @@ module.exports = {
                 "z": 0
             }
         }
-
-
     },
-    "dates": {
-        "2016-11-01": [
-            {
-                "type": "model",
-                "title": "First forest model",
-                "description": "Initial 3D model",
-                "location": "origin",
-                "source": "16-11-temple.gltf"
-            }
-        ],
-        "2016-12-01": [
-            {
-                "type": "model",
-                "title": "Design iteration model",
-                "description": "Initial 3D model",
-                "location": "origin",
-                "source": "16-12-temple.gltf"
-            }
-        ],
-        "2017-01-01": [
-            {
-                "type": "model",
-                "title": "Design iteration model",
-                "description": "Initial 3D model",
-                "location": "origin",
-                "source": "17-01-temple.gltf"
-            }
-        ],
-        "2017-02-01": [
-            {
-                "type": "model",
-                "title": "Design iteration model",
-                "description": "Initial 3D model",
-                "location": "origin",
-                "source": "17-02-temple.gltf"
-            }
-        ],
-        "2017-05-01": [
-            {
-                "type": "model",
-                "title": "Design iteration model",
-                "description": "test2",
-                "location": "origin",
-                "source": "somesorce.gltf"
-            }
-        ],
-        "2017-06-01": [
-            {
-                "type": "model",
-                "title": "Design iteration model",
-                "description": "test3",
-                "location": "origin",
-                "source": "somesorce.gltf"
-            }
-        ],
-        "2017-07-01": [
-            {
-                "type": "model",
-                "title": "Design iteration model",
-                "description": "test4",
-                "location": "origin",
-                "source": "somesorce.gltf"
-            }
-        ],
-        "2017-08-01": [
-            {
-                "type": "model",
-                "title": "Design iteration model",
-                "description": "test5",
-                "location": "origin",
-                "source": "17-08-temple.gltf"
-            }
-        ],
+    "models": {
+        "2016-11-01": 
+        {
+            "title": "First forest model",
+            "description": "Initial 3D model",
+            "location": "origin",
+            "source": "16-11-temple.gltf"
+        },
+        "2016-12-01": 
+        {
+            "title": "Design iteration model",
+            "description": "Initial 3D model",
+            "location": "origin",
+            "source": "16-12-temple.gltf"
+        },
+        "2017-01-01": 
+        {
+            "title": "Design iteration model",
+            "description": "Initial 3D model",
+            "location": "origin",
+            "source": "17-01-temple.gltf"
+        },
+        "2017-02-01": 
+        {
+            "title": "Design iteration model",
+            "description": "Initial 3D model",
+            "location": "origin",
+            "source": "17-02-temple.gltf"
+        },
+        "2017-05-01": 
+        {
+            "title": "Design iteration model",
+            "description": "test2",
+            "location": "origin",
+            "source": "somesorce.gltf"
+        },
+        "2017-06-01": 
+        {
+            "title": "Design iteration model",
+            "description": "test3",
+            "location": "origin",
+            "source": "somesorce.gltf"
+        },
+        "2017-07-01": 
+        {
+            "title": "Design iteration model",
+            "description": "test4",
+            "location": "origin",
+            "source": "somesorce.gltf"
+        },
+        "2017-08-01": 
+        {
+            "title": "Design iteration model",
+            "description": "test5",
+            "location": "origin",
+            "source": "17-08-temple.gltf"
+        }
+    },
+    "360images": {    
         "2017-08-15": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-1",
                 "description": "test6",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -273,8 +250,7 @@ module.exports = {
         ],
         "2017-08-16": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-2",
                 "description": "test7",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -282,8 +258,7 @@ module.exports = {
         ],
         "2017-08-17": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-3",
                 "description": "test8",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -291,8 +266,7 @@ module.exports = {
         ],
         "2017-08-18": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-4",
                 "description": "test9",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -300,8 +274,7 @@ module.exports = {
         ],
         "2017-08-19": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-5",
                 "description": "Initial 3D model",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -309,15 +282,7 @@ module.exports = {
         ],
         "2017-08-20": [
             {
-                "type": "model",
-                "title": "Design iteration model",
-                "description": "Initial 3D model",
-                "location": "origin",
-                "source": "somesorce.gltf"
-            },
-            {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-6",
                 "description": "Initial 3D model",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -325,8 +290,7 @@ module.exports = {
         ],
         "2017-08-21": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-7",
                 "description": "Initial 3D model",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -334,8 +298,7 @@ module.exports = {
         ],
         "2017-08-22": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-8",
                 "description": "Initial 3D model",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -343,8 +306,7 @@ module.exports = {
         ],
         "2017-08-23": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-9",
                 "description": "Initial 3D model",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -352,8 +314,7 @@ module.exports = {
         ],
         "2017-08-24": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-10",
                 "description": "Initial 3D model",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -361,8 +322,7 @@ module.exports = {
         ],
         "2017-08-25": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-11",
                 "description": "Initial 3D model",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -370,8 +330,7 @@ module.exports = {
         ],
         "2017-08-26": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-12",
                 "description": "Initial 3D model",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -379,8 +338,7 @@ module.exports = {
         ],
         "2017-08-27": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-13",
                 "description": "Initial 3D model",
                 "location": "origin",
                 "source": "somesorce.jpg"
@@ -388,7 +346,6 @@ module.exports = {
         ],
         "2017-08-28": [
             {
-                "type": "360",
                 "title": "Design iteration model",
                 "description": "Initial 3D model",
                 "location": "main01",
@@ -397,8 +354,7 @@ module.exports = {
         ],
         "2017-09-02": [
             {
-                "type": "360",
-                "title": "Design iteration model",
+                "title": "360-test-14",
                 "description": "Initial 3D model",
                 "location": "origin",
                 "source": "somesorce.jpg"
