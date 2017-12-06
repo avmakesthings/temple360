8aacc8f3fcccc672c87d7f1a99447795cc9cc093
AV <av@anastasia.io>
2017-12-06 14:49:33 +1100

    tweaked text panel a little but still needs work to support text paragraphs

--- js/mainData.js
+++ js/mainData.js
@@ -247,7 +247,7 @@ module.exports = {
         "2017-07-01": 
         {
             "title": "Refined model",
-            "description": "test5",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
             "location": "origin",
             "source": "17-07-temple.gltf"
         },
